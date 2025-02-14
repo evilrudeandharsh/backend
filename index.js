ce2444a34b01061d7762c493163ec57126ab8ca8
Aadi Vinayak <aadivinayak36@gmail.com>
2025-02-14 14:24:06 +0530

    initialize project with basic Express server and dotenv configuration

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3125
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter',(req,res)=>{
+    res.send('Twitter')
+})
+
+app.get('/login',(req,res)=>{
+    res.send('<h1>  Login Page </h1>')
+})
+app.get('/youtube',(req,res)=>{
+    res.send('<h2>  youtubeee </h1>')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
